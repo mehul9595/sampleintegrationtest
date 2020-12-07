e916eeb1f3558b296d0009e4d3e253f143bf3357
mehul9595 <mehul9595@gmail.com>
2020-12-07 14:29:26 +0800

    commit test project for unit testing

---
+++ src/SampleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using Microsoft.Extensions.Configuration;
+
+namespace SampleApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var config = new ConfigurationBuilder().AddJsonFile("config.json").Build();
+            var fileContent = config["FileContent"];
+
+            var tempFolder = Path.GetTempPath();
+            var filePath = Path.Combine(tempFolder, "testfile.txt");
+            File.WriteAllText(filePath, fileContent);
+        }
+    }
+}
